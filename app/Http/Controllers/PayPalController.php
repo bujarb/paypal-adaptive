a79f86d69b06c4735fa7c325cc68cfd46caf34fc
Bujar Begisholli <begisholli.bujar@gmail.com>
2018-06-20 17:44:28 +0200

    initial commit

---
+++ app/Http/Controllers/PayPalController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PayPalController extends Controller
+{
+    public function __construct(){
+
+    }
+
+    public function getPaymentOptions($payKey){
+
+    }
+
+    public function setPaymentDetails(){
+
+    }
+
+    public function createPayRequest(){
+
+    }
+
+    public function __paypalSend($data, $call){
+
+    }
+
+    public function splitPay(){
+        
+    }
+}
